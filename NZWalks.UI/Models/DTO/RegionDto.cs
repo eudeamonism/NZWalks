ee283cc86b973817547b7e059af3c903e6944ac8
E V <evanvargas@gmail.com>
2023-06-23 17:12:37 -0500

    CRUD API

--- NZWalks.UI/Models/DTO/RegionDto.cs
+++ NZWalks.UI/Models/DTO/RegionDto.cs
@@ -3,8 +3,11 @@
     public class RegionDto
     {
         public Guid Id { get; set; }
+
         public string Code { get; set; }
+
         public string Name { get; set; }
+
         public string? RegionImageUrl { get; set; }
     }
 }
